e7e3d1709ee1c2306ea4306f30bff37b319491ab
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-03-21 16:50:22 -0300

    validaciones y estatus

--- src/main/resources/static/assets/index.js
+++ src/main/resources/static/assets/index.js
@@ -26,8 +26,30 @@ document.getElementById("subir").addEventListener("click", function(event) {
     event.preventDefault();
     const size = document.getElementById("size").value;
     const archivo = document.getElementById("archivo").files[0];
+    const statusMessage = document.getElementById('statusMessage');
+
+    if (!archivo) {
+        statusMessage.textContent = "Faltan adjuntar archivo";
+        statusMessage.className = "error";
+        throw new Error("Faltan adjuntar archivo");
+    }
+
+    if (!size) {
+        statusMessage.textContent = "Faltan indicar tamaño";
+        statusMessage.className = "error";
+        throw new Error("Faltan indicar tamaño");
+    }
+
+    if (archivo.size < size) {
+        statusMessage.textContent = "El tamaño excede el del archivo";
+        statusMessage.className = "error";
+        throw new Error("El tamaño excede el del archivo");
+    }
+
     const formData = new FormData();
     formData.append("file", archivo);
+    statusMessage.textContent = "Procesando Archivo...";
+    statusMessage.className = "info";
     fetch(`/api/upload?size=${size}`, {
         method: "POST",
         body: formData,
@@ -36,19 +58,29 @@ document.getElementById("subir").addEventListener("click", function(event) {
             if(res.status == 413) {
                 alert("Tamaño del archivo demasiado grande");
                 throw new Error("Tamaño archivo excede el límite");
+                statusMessage.textContent = "Tamaño archivo excede el límite";
+                statusMessage.className = "error";
             }
+            statusMessage.textContent = "Archivo procesado exitosamente.";
+            statusMessage.className = "success";
             return res.json()
         })
         .catch(res => {
-            console.log(res)
+            console.error(res)
+            statusMessage.textContent = "Hubo un error al procesar el archivo.";
+            statusMessage.className = "error";
         })
         .then(res => {
             if(!res) {
                 throw new Error("Sin respuesta del servidor");
+                statusMessage.textContent = "Sin respuesta del servidor";
+                statusMessage.className = "error";
             }
             actualizarLista(res, archivo.name)
         })
         .catch(error => {
             console.log("Error:", error)
+            statusMessage.textContent = error;
+            statusMessage.className = "error";
         });
 });
